61d9c1216af078dacfe701ba339b00cd94dc4d1c
savitamali02 <savitam247@gmail.com>
2025-06-11 11:30:58 +0530

    second commit

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,19 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_APIKEY,
   authDomain: import.meta.env.VITE_AUTHDOMAIN,
-  projectId: import.meta.env.VITE_PROJECTEDID,
+  projectId: import.meta.env.VITE_PROJECTID, // ✅ Fixed typo here
   storageBucket: import.meta.env.VITE_STORAGEBUCKET,
-  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
-  appId:import.meta.env.VITE_APPID
+  messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+  appId: import.meta.env.VITE_APPID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
-export default app ;
+export default app;
